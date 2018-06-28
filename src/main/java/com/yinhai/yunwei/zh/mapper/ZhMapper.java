641f16bb1196b00b50356dc116c1050bfd2b895e
范超 <707293891@qq.com>
2018-06-28 11:02:12 +0800

    Update ZhMapper.java

--- src/main/java/com/yinhai/yunwei/zh/mapper/ZhMapper.java
+++ src/main/java/com/yinhai/yunwei/zh/mapper/ZhMapper.java
@@ -20,10 +20,6 @@ public interface ZhMapper {
     List<Map> getAllTables(@Param(value = "database") String database);
 
 
-    @Select("select name,info,concat('人员基础信息表(A_PERSONAL_BASE)：',baseData,convert(char(10),char),'人员基础信息表(A_PERSONAL_BASE_YINHAI)：',yinhaiData) as dataInfo,\n" +
-            "note from ( select '纵横数据交换数据库' as name ,'只有人员基础数据' as info,\n" +
-            "concat('纵横mysql数据表情况：',convert(char(10),char),'目前只有人员基础信息，暂无工会和单位数据') as note,\n" +
-            "(select count(1) from A_PERSONAL_BASE a) as baseData,\n" +
-            "(select count(1) from A_PERSONAL_BASE_YINHAI b) as yinhaiData ) a")
+    @Select("**************")
     YunweiInfo count();
 }
